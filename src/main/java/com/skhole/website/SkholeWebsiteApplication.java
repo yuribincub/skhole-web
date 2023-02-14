8e9dbbe6ebba878f038195da469a41739804a8db
yuri <yuribincub@gmail.com>
2023-02-14 10:36:32 -0500

    Initial commit

---
+++ src/main/java/com/skhole/website/SkholeWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.skhole.website;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkholeWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkholeWebsiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skhole/website/SkholeWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skhole.website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkholeWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
